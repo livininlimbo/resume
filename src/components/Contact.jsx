15c1b7619e595424a5f5be58fac35636d3d0c574
LIMBO <livininlimboyt@gmail.com>
2025-01-25 15:29:07 -0800

    Fixed Submit Button Size on Mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,7 +26,7 @@ export const Contact = () => {
             <input type="text" name="to_name" placeholder="Name" required className="md:w-1/2 lg:w-1/2 p-3 m-2 text-base font-thin text-stone-900 dark:text-white bg-white dark:bg-black border rounded-lg border-black dark:border-white  sm:w-2/3 xs:w-2/3  "/>
             <input type="email" name="email" placeholder="Email" required className="md:w-1/2 lg:w-1/2 p-3 m-2 text-base font-thin text-stone-900 dark:text-white bg-white dark:bg-black border rounded-lg border-black dark:border-white sm:w-2/3 xs:w-2/3"/>
             <textarea placeholder="Message" name="message" required className="md:w-1/2 lg:w-1/2 p-3 m-2 text-base font-thin text-stone-900 dark:text-white bg-white dark:bg-black border rounded-lg border-black dark:border-white sm:w-2/3 xs:w-2/3"/>
-            <button type="submit" value="Send" className="md:w-[12%] lg:w-[18%] p-3 m-2 mt-8 text-base font-bold text-stone-900 dark:text-white bg-white dark:bg-black transition-all duration-200 hover:bg-gray-200 dark:hover:bg-stone-900 border rounded-lg border-black dark:border-white mb-12 sm:w-1/6 xs:w-1/6">Send</button>
+            <button type="submit" value="Send" className="md:w-[12%] lg:w-[18%] p-3 m-2 mt-8 text-base font-bold text-stone-900 dark:text-white bg-white dark:bg-black transition-all duration-200 hover:bg-gray-200 dark:hover:bg-stone-900 border rounded-lg border-black dark:border-white mb-12 sm:w-1/5 xs:w-1/5">Send</button>
             {/* </div> */}
         </form>   
     )
